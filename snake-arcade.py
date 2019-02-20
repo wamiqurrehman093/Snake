3389e3d86ae057cc030010e83c797f65ad19d21a
Wamiq <wamiqurehman093@gmail.com>
2019-02-20 08:31:04 +0500

    somecode

--- snake-arcade.py
+++ snake-arcade.py
@@ -10,10 +10,14 @@ BLACK = arcade.color.BLACK
 WHITE = arcade.color.WHITE
 RED = arcade.color.RED
 
+GRID = 16
+COUNT = 0
+
+
 class Food:
-    def __init__(self, position):
-        self.x = position[0]
-        self.y = position[1]
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
         self.radius = 4
         self.color = RED
         self.timer = 0.0
@@ -27,79 +31,41 @@ class Food:
         arcade.draw_circle_filled(self.x, self.y, self.radius, self.color)
 
 class Unit:
-    def __init__(self, position):
-        self.x = position[0]
-        self.y = position[1]
-        self.width = 17
-        self.height = 17
-        self.color = WHITE
+    def __init__(self):
+        self.color = RED
 
-    def move(self, dirnx, dirny):
-        self.x += dirnx
-        self.y += dirny
-
-    def draw(self):
-        arcade.draw_rectangle_filled(self.x, self.y, self.width, self.height,
-                                    self.color)
 class Snake:
-    def __init__(self, position):
-        self.body = []
-        head = Unit(position)
-        self.body.append(head)
-        self.dirnx = 0
-        self.dirny = 1
-        self.key = None
-        
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        # snake velocity. moves one grid length every frame in either the x or y direction
+        self.dx = GRID
+        self.dy = 0
+        # keep track of all grids the snake body occupies
+        self.cells = []
+        # length of the snake. grows when eating an apple
+        self.maxCells = 4
+
     def move(self):
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                pygame.quit()
+        self.x += = self.dx
+        self.y += = self.dy
 
-            keys = pygame.key.get_pressed()
+        # check horizontally edges
+        if self.x < 0:
+            self.x = WIDTH - GRID
+        elif self.x > WIDTH:
+            self.x = 0
+        # check vertically edges
+        if self.y < 0:
+            self.y = HEIGHT - GRID
+        elif self.y > HEIGHT:
+            self.y = 0
 
-            for key in keys:
-                if keys[pygame.K_LEFT]:
-                    self.dirnx = -1
-                    self.dirny = 0
-                    self.turns[self.head.pos[:]] = [self.dirnx, self.dirny]
+    def update(self):
+        self.cells.append({x: self.x, y: self.y})
+        if len(self.cells) > self.maxCells:
+            self.cells.pop()
 
-                elif keys[pygame.K_RIGHT]:
-                    self.dirnx = 1
-                    self.dirny = 0
-                    self.turns[self.head.pos[:]] = [self.dirnx, self.dirny]
-
-                elif keys[pygame.K_UP]:
-                    self.dirnx = 0
-                    self.dirny = -1
-                    self.turns[self.head.pos[:]] = [self.dirnx, self.dirny]
-
-                elif keys[pygame.K_DOWN]:
-                    self.dirnx = 0
-                    self.dirny = 1
-                    self.turns[self.head.pos[:]] = [self.dirnx, self.dirny]
-
-        for i, c in enumerate(self.body):
-            p = c.pos[:]
-            if p in self.turns:
-                turn = self.turns[p]
-                c.move(turn[0],turn[1])
-                if i == len(self.body)-1:
-                    self.turns.pop(p)
-            else:
-                if c.dirnx == -1 and c.pos[0] <= 0:
-                    c.pos = (c.rows-1, c.pos[1])
-                elif c.dirnx == 1 and c.pos[0] >= c.rows-1:
-                    c.pos = (0,c.pos[1])
-                elif c.dirny == 1 and c.pos[1] >= c.rows-1:
-                    c.pos = (c.pos[0], 0)
-                elif c.dirny == -1 and c.pos[1] <= 0:
-                    c.pos = (c.pos[0],c.rows-1)
-                else:
-                    c.move(c.dirnx,c.dirny)
-
-    def draw(self):
-        for unit in self.body:
-            unit.draw()
 
 class Window(arcade.Window):
     def __init__(self, width, height, title):
@@ -113,8 +79,9 @@ class Window(arcade.Window):
         self.snake.draw()
     def setup(self):
         arcade.set_background_color(arcade.color.BLACK)
-        self.food = Food((100, 250))
-        self.snake = Snake((300, 200))
+        self.food = Food(100, 250)
+        self.snake = Snake(300, 200)
+
     def update(self, delta_time):
         self.food.timer += delta_time
         seconds = int(self.food.timer) % 60
